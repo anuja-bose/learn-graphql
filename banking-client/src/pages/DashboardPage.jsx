29f4a721684c21c795d2d6f16b7c2ae08bbd6838
anuja.bose <anuja.joydeep.bose@gmail.com>
2022-07-25 17:01:28 +0530

    FE : Unwanted code removed

--- banking-client/src/pages/DashboardPage.jsx
+++ banking-client/src/pages/DashboardPage.jsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from 'react';
-
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import AccountDataTable from '../components/accounts/AccountDataTable';
-import AccountsList from '../components/accounts/AccountsList'
 import { useQuery, gql } from '@apollo/client';
 
 
@@ -16,11 +13,8 @@ const AccountsPage = () => {
     }
   }
 `;
-  const { loading, error, data } = useQuery(GET_ACCOUNT_USER);
-  console.log('loading', loading);
-  console.log('data.accountUser', data);
-  console.log('erroe', error);
 
+  const { loading, error, data } = useQuery(GET_ACCOUNT_USER);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error :(</p>;
